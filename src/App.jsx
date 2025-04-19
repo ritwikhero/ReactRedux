1ba01f32843d3f5c50aca20de101d82254d922a4
Ritwik <ritwikhero@gmail.com>
2025-04-19 23:23:18 +0530

    redux setup done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import 
+
+function App() {
+  return <>hello world</>;
+}
+
+export default App;
---
+++ src/components/redux/pizza/PizzaActions.jsx
@@ -0,0 +1,6 @@
+import { ORDER_PIZZA } from "./PizzaTypes";
+export const orderPizza = () => {
+  return {
+    type: ORDER_PIZZA,
+  };
+};
